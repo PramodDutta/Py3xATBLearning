1e579b9a314a1cad7a150220e6825bc94c41e78b
Promode <pramoddutta_96@yahoo.com>
2024-06-12 08:07:26 +0530

    all files added

---
+++ ex01_June/ex_12062024/Lab047.py
@@ -0,0 +1,10 @@
+age = 30
+name = "Pramod"
+
+if name == "Pramod":
+    if age == 30:
+        print("Allowed")
+    else:
+        print("Not Allowed!, wrong age")
+else:
+    print("Wrong Name")
---
+++ ex01_June/ex_12062024/Lab048.py
@@ -0,0 +1,4 @@
+age = 90
+
+if age > 100:
+    print("You are old")
